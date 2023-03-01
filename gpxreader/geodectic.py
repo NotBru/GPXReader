6e32c05160f3452715d89dbe34ca6e870c42ff0c
Bru Ronchi <brunom.ronchi@gmail.com>
2023-03-01 10:46:15 -0300

    Fix sqrt receiving invalid values

--- gpxreader/geodectic.py
+++ gpxreader/geodectic.py
@@ -15,6 +15,22 @@ def wgs84_to_cartesian(
     longitude: np.array | pd.Series,
     elevation: np.array | pd.Series | float | int,
 ) -> dict[str, np.array | series]:
+    """Convert WGS84 coordinates to cartesian coordinates
+
+    Parameters
+    ----------
+    latitude : np.array | pd.Series
+        Latitude array
+    longitude : np.array | pd.Series
+        Longitude array
+    elevation : np.array | pd.Series | float | int
+        Elevation array or, if constant, scalar
+
+    Returns
+    -------
+    dict[str, np.array | series] :
+        dict with "x", "y", and "z" keys, and their corresponding series
+    """
 
     latitude = deg_to_rad * latitude
     longitude = deg_to_rad * longitude
@@ -41,11 +57,41 @@ def wgs84_to_cartesian(
         "z": (M + elevation) * np.sin(latitude),
     }
 
-def wgs84_to_displacements(
+def process_wgs84(
     latitude: np.array | pd.Series,
     longitude: np.array | pd.Series,
     elevation: np.array | pd.Series | float | int,
+    smoothing: (int, int, int) = (0, 0, 0)
 ) -> dict[str, np.array | series]:
+    """Obtain cartesian coordinates and displacements. Smoothing is performed
+    beforehand.
+
+    Parameters
+    ----------
+    latitude : np.array | pd.Series
+        Latitude array
+    longitude : np.array | pd.Series
+        Longitude array
+    elevation : np.array | pd.Series | float | int
+        Elevation array or, if constant, scalar
+    smoothing : (int, int, int)
+        Length of smoothing kernel for lat, lon, and elevation series.
+    """
+
+    def smoothen(a: np.array | pd.Series, l: int):
+        if l <= 1:
+            return a
+        x = np.linspace(-2, 2, l)
+        ker = np.exp(-.5 * x ** 2)
+        ker /= np.sum(ker)
+        convolution = np.convolve(a, ker, mode="same")
+        if isinstance(a, np.array):
+            return convolution
+        return pd.Series(convolution, index=a.index, name=a.name)
+
+    latitude = smoothen(latitude, smoothing[0])
+    longitude = smoothen(longitude, smoothing[1])
+    elevation = smoothen(elevation, smoothing[2])
 
     r = wgs84_to_cartesian(latitude, longitude, elevation)
 
@@ -59,7 +105,8 @@ def wgs84_to_displacements(
     displacement = np.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
 
     return {
-        "horizontal_displacement": np.sqrt(displacement ** 2 - de ** 2),
+        **r,
+        "horizontal_displacement": np.sqrt(np.max(displacement ** 2 - de ** 2, 0)),
         "vertical_displacement": de,
         "displacement": displacement,
     }
